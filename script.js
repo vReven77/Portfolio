90f7d1f0cb818da48c468509c2e503b24fda9828
vReven77 <132178788+vReven77@users.noreply.github.com>
2023-04-30 04:24:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function closeSmallNavbar() {
+    document.getElementById("navbar-small-menu").style.display = "none";
+}
+
+function openSmallNavbar() {
+    document.getElementById("navbar-small-menu").style.display = "flex";
+}
